b22c1ea17b29e5ab43ab17a6986bbe3a1ce3f53e
Russell Loewe <russloewe@gmail.com>
2018-07-31 09:21:30 -0700

    added dialog box tests

--- test/test_trend_mapper_dialog.py
+++ test/test_trend_mapper_dialog.py
@@ -41,21 +41,70 @@ class TrendMapperDialogTest(unittest.TestCase):
         result = self.dialog.result()
         self.assertEqual(result, QDialog.Accepted)
         
-    def test_setAttributeCombos(self):
-        testList = ['one', 'two', 'three']
-        self.dialog.setLayerAttributesCombos(testList)
-        for n in range(3):
-            self.assertEqual(self.dialog.CatagoryCombo.findText(testList[n]) >= 0, True)
-        testList2 = ['four', 'five', 'six']
-        for n in range(3):
-            self.assertEqual(self.dialog.CatagoryCombo.findText(testList[n]) < 0, True)
-
     def test_dialog_cancel(self):
         """Test we can click cancel."""
         button = self.dialog.button_box.button(QDialogButtonBox.Cancel)
         button.click()
         result = self.dialog.result()
         self.assertEqual(result, QDialog.Rejected)
+        
+    def test_setLayerAttributeCombos(self):
+        '''test that we can update the three different layer 
+        attribute combos'''
+        testList = ['one', 'two', 'three']
+        self.dialog.setLayerAttributesCombos(testList)
+        for n in range(3):
+            self.assertEqual(self.dialog.CatagoryCombo.findText(testList[n]) >= 0, True)
+            self.assertEqual(self.dialog.XFieldCombo.findText(testList[n]) >= 0, True)
+            self.assertEqual(self.dialog.YFieldCombo.findText(testList[n]) >= 0, True)
+        testList2 = ['four', 'five', 'six']
+        self.dialog.setLayerAttributesCombos(testList2)
+        for n in range(3):
+            self.assertEqual(self.dialog.CatagoryCombo.findText(testList[n]) < 0, True)
+            self.assertEqual(self.dialog.XFieldCombo.findText(testList[n]) < 0, True)
+            self.assertEqual(self.dialog.YFieldCombo.findText(testList[n]) < 0, True)
+
+    def test_setLayerInputCombo(self):
+        '''same test as the attribute combo test but for the layer
+        input combo'''
+        testList = ['one', 'two', 'three']
+        self.dialog.setLayerInputCombo(testList)
+        for n in range(3):
+            self.assertEqual(self.dialog.CatagoryCombo.findText(testList[n]) >= 0, True)
+        testList2 = ['four', 'five', 'six']
+        self.dialog.setLayerInputCombo(testList2)
+        for n in range(3):
+            self.assertEqual(self.dialog.CatagoryCombo.findText(testList[n]) < 0, True)
+
+    def test_getInputCombo(self):
+        '''test that the right layer is returned for the getInputLayer '''
+        testList = ['one', 'two', 'three']
+        self.dialog.setLayerInputCombo(testList)
+        self.dialog.CatagoryCombo.setCurrentIndex(0) 
+        self.assertEqual(self.dialog.getInputLayer(), 'one')
+        self.dialog.CatagoryCombo.setCurrentIndex(1) 
+        self.assertEqual(self.dialog.getInputLayer(), 'two')
+        self.dialog.CatagoryCombo.setCurrentIndex(2) 
+        self.assertEqual(self.dialog.getInputLayer(), 'three')
+        
+    def test_checkboxGetters(self):
+        '''test all of the checkbox getter functions'''
+        self.dialog.DiscardBadFitCheck.setChecked(True)
+        self.dialog.FilterOutliersCheck.setChecked(True)
+        self.dialog.ExportRisidualsCheck.setChecked(True)
+        
+        self.assertEqual(self.dialog.getDiscardBadFitOption(), True)
+        self.assertEqual(self.dialog.getExportRisidualsOption(), True)
+        self.assertEqual(self.dialog.getFilterOutliersOption(), True)
+
+        self.dialog.DiscardBadFitCheck.setChecked(False)
+        self.dialog.FilterOutliersCheck.setChecked(False)
+        self.dialog.ExportRisidualsCheck.setChecked(False)
+        
+        self.assertEqual(self.dialog.getDiscardBadFitOption(), False)
+        self.assertEqual(self.dialog.getExportRisidualsOption(), False)
+        self.assertEqual(self.dialog.getFilterOutliersOption(), False)
+    
 
 if __name__ == "__main__":
     suite = unittest.makeSuite(TrendMapperDialogTest)
