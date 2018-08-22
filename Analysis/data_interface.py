bfc7bc93566bfbb5fbb400c1efd23aeeaadc8a9e
Russell Loewe <russloewe@gmail.com>
2018-08-22 15:40:06 -0700

    rewrote data interface to use sql, major overhaul, still working on tests and interface, but good stopping point

--- Analysis/data_interface.py
+++ Analysis/data_interface.py
@@ -1,188 +1,83 @@
-import csv
-import numpy
-from sets import Set
+import csv, sqlite3
 import os
 
 class DataInterface():
+    
     def __init__(self):
-        self.csvFiles = []# list of csv file objects
-        self.QGIS = []
-        self.Arch = []
-        self.categoryLable = None
-        self.xLable = None
-        self.yLable = None
-        self.copyAttributeLables = []
-        self.cvsCategoryIndex = {}
+        self.attributeNames = []
+    
+    def test(self):
+        pass
+        
+    def setAttributeNames(self, names):
+        '''add attributes to an array that will be copied from 
+        the input CSV to the SQL database'''
+        for name in names:
+            if name not in self.attributeNames:
+                self.attributeNames.append(name)
+        self.attributeNamesTuple = tuple(self.attributeNames)
+    
+    def initSQL(self):
+        self.con = sqlite3.connect(":memory:")
+        self.con.enable_load_extension(True)
+        curR = self.con.cursor()
+        if len(self.attributeNames) < 1:
+            raise AttributeError("no attributes specified, cant init SQL")
+        curR.execute("CREATE TABLE CSVdata (dummy int)")
+        for name in self.attributeNames:
+            curR.execute("ALTER TABLE CSVdata ADD {} VARCHAR(50)".format(name))
+        self.con.commit()
 
+    def loadCSV(self, filePath):
+        '''take the path of a csv file and import the rows into a sql 
+        database'''
+        try:
+            cur = self.con.cursor()
+        except AttributeError:
+            raise AttributeError('SQL connection not established')
+        with open(filePath, 'rb') as csvfile:
+            dr = csv.DictReader(csvfile) # comma is default delimiter
+            for i in dr:
+                to_db = []
+                try:
+                    for name in self.attributeNames:
+                        to_db.append(i[name])
+                except KeyError:
+                    return False #tell the calling function that the file cant load
+                sqlVals = tuple(to_db)
+                sqlStatement = "INSERT INTO CSVdata {} VALUES {};".format(self.attributeNamesTuple, sqlVals)
+                cur.execute(sqlStatement)
+        self.con.commit()
+        return True
+    
+    def writeDataOut(self, path):
+         with open(path, 'w') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            #write header to first line, self.unk is original unique key
+            header = [f for f in self.dataSets[0].dataStats]          
+            spamwriter.writerow(header) 
+            for data in self.dataSets:
+                row = [data.dataStats[key] for key in header]
+                spamwriter.writerow(row) 
     
     def loadFolder(self, folderPath):
         '''Get a list of csv files in the provided directory and
             add each file with the addCsvFile function'''
         for file in os.listdir(folderPath):
             if file.endswith(".csv"):
-                self.addCSVFile(os.path.join(folderPath, file))
-
-
-    def addCSVFile(self, fileName):
-        '''Create a new csv file object and add it to an array '''
-        newF = CSVFile(fileName)
-        self.csvFiles.append(newF)
-        
-    def getCSVFileList(self, filterInvalid=True):
-        '''Return a list of the files that have been added with
-            the option to filter out the files that dont have 
-            valid attributes'''
+                self.loadCSV(os.path.join(folderPath, file))
             
-        fileList = []
-        for f in self.csvFiles:
-            if filterInvalid:
-                if f.validLables:
-                    fileList.append(f.path)
-            else:
-                fileList.append(f.path)
-        return fileList
-            
-    def setCategoryLable(self, name):
-        self.categoryLable = name
-        
-    def setXLable(self, lable):
-        self.xLable = lable
-        
-    def setYLable(self, lable):
-        self.yLable = lable
-        
-    def addCopyAttributeLable(self, lable):
-        if lable not in self.copyAttributeLables:
-            self.copyAttributeLables.append(lable)
-        
-
-    
-    def indexCategories(self):
-        '''iterat through all the CSV files in the list,
-            read every line and record every unique name and add to a 
-            dict that has the name and what files it was found in'''
-        nameList = {}
-        count = 0
-        for fileObj in [f for f in self.csvFiles if f.validLables]:
-            count += 1
-            fileName = fileObj.path
-            categoryIndex = fileObj.getIndex(self.categoryLable)
-            with open(fileName, 'rb') as csvfile:
-                spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-                for row in spamreader:
-                    catVal = row[categoryIndex]
-                    if catVal != self.categoryLable:
-                        if catVal not in nameList:
-                            nameList[catVal] = [fileName]
-                        else:
-                            if fileName not in nameList[catVal]:
-                                nameList[catVal].append(fileName)
-        self.cvsCategoryIndex = nameList
-        
-    def getCategoryList(self):
-        catList = []
-        for station in list(self.cvsCategoryIndex.keys()):
-            catList.append(station)
-        return catList
-                    
-        #call get csv name date
-    def getCategoryDataset(self, uniqueName):
-        dataPoints = []
-        dataObj = DataSeries(uniqueName)
-        for fileObj in [fil for fil in self.csvFiles if fil.path in self.cvsCategoryIndex[uniqueName]]:
-            fileName = fileObj.path
-            nameInd = fileObj.getIndex(self.categoryLable)
-            #print nameInd, uniqueName, fileName
-           # print fileObj.indexDict
-            
-            xInd = fileObj.getIndex(self.xLable)
-            yInd =  fileObj.getIndex(self.yLable)
-            with open(fileName, 'rb') as csvfile:
-                spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-                for row in spamreader:
-                    if row[nameInd] == uniqueName:
-                        #print uniqueName,row[xInd], row[yInd]
-                        dataObj.addDataPoint(row[xInd], row[yInd])
-                        for attr in self.copyAttributeLables:
-                            if attr not in dataObj.copyAtttributes: #try to avoid direct access
-                                try:
-                                    dataObj.addCopyAttribute(attr, row[fileObj.getIndex(attr)])
-                                except ValueError:
-                                    pass
-                        
-                        #add copy data
-        return dataObj
-
-    def getFileCountAll(self):
-        c = 0
-        for i in self.csvFiles:   
-            c += 1
-        return c
-        
-    def getFileCountValid(self):
-        c = 0
-        for i in self.csvFiles:
-            if i.validLables:
-                c += 1
-        return c
-        
-    def indexCSVFiles(self):
-        '''Takes array of the first line of CSV file and
-            locates the indices of the three attribute columns
-            we need for analysis'''
-        if (self.categoryLable == None) or (self.xLable == None) or (self.yLable == None):
-            raise ValueError('cant index files without lables set')
-        for fileObj in self.csvFiles:
-            fileName = fileObj.path
-            with open(str(fileName), 'rb') as csvfile:
-                spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-                for row in spamreader:
-                    try:
-                        fileObj.addIndex(self.categoryLable, row.index(self.categoryLable))
-                        fileObj.addIndex(self.xLable, row.index(self.xLable))
-                        fileObj.addIndex(self.yLable, row.index(self.yLable))
-                        for name in self.copyAttributeLables:
-                            if (name in row):
-                                fileObj.addIndex(name, row.index(name))
-                    except ValueError:
-                        fileObj.validLables = False
-                    break
-class DataSeries():
-    def __init__(self, name, xLable = 'x', yLable = 'y'):
-        '''init the data object'''
-        self.name = name
-        self.xLable = xLable
-        self.yLable = yLable
-        self.copyAtttributes = {}
-        self.dataPoints = []
-        self.linearRegCoef = [] #[slope, intercept]
-        #stats 
-        self.zScore = None
-        self.risidualSum = None
-
-    def addDataPoint(self, xVal, yVal):
+    def pullUniqueKeys(self, column):
+        ''' grab a list of all the unique names in the given
+        column'''
+        keyList = []
         try:
-            xVal = float(xVal)
-            yVal = float(yVal)
-        except Exception:
-           # print 'cant make ({}, {}) floats'.format(xVal, yVal)
-            return
-        self.dataPoints.append((xVal, yVal))
+            cur = self.con.cursor()
+        except AttributeError:
+            raise AttributeError('SQL connection not established')
+        cur.execute('SELECT DISTINCT {} from CSVdata'.format(column))
+        for row in cur:
+            keyList.append(row)
+        return keyList
         
-    def addCopyAttribute(self, attrLable, attrVal):
-        if attrLable not in self.copyAtttributes:
-            self.copyAtttributes[attrLable] = attrVal
-
-class CSVFile():
-    def __init__(self, path):
-        self.path = path
-        self.indexDict = {}
-        self.validLables = True
-    
-    def addIndex(self, lable, loc):
-        if lable not in self.indexDict:
-            self.indexDict[lable] = loc
-            
-    def getIndex(self, lable):
-        if lable in self.indexDict:
-            return self.indexDict[lable]
--- Analysis/test/test_datainterface.py
+++ Analysis/test/test_datainterface.py
@@ -23,64 +23,22 @@ class DataInterfaceTest(unittest.TestCase):
     def setUp(self):
         """Runs before each test."""
         self.interface = DataInterface()
+        names = ['DATE', 'STATION', 'TAVG', 'NAME', 'LATITUDE', 'LONGITUDE']
+        self.interface.setAttributeNames(names)
+        self.interface.initSQL()
         self.interface.loadFolder('./Analysis/test/')
-        self.interface.setCategoryLable('STATION')
-        self.interface.setXLable('DATE')
-        self.interface.setYLable('TAVG')
-        self.interface.addCopyAttributeLable('LATITUDE')
-        self.interface.indexCSVFiles()
-        self.interface.indexCategories()
 
     def tearDown(self):
         """Runs after each test."""
         self.interface = None
         
-    def test_copyAttributes(self):
-        '''Make sure the copy attirbute lable loaded correctly'''
-        for f in self.interface.csvFiles:
-            if f.validLables:
-                self.assertTrue('LATITUDE' in f.indexDict.keys())
-    
-    def test_categoryIndexing(self):
-        '''Make sure the category indexer works'''
-        fcount = len(self.interface.cvsCategoryIndex['USR0000OECK'])
-        self.assertTrue(fcount == 3)
-            
-    def test_loadFolder(self):
-        '''Test load folder...'''
-        fileList = self.interface.getCSVFileList(filterInvalid=False)
-        self.assertEqual(len(fileList) , 6)
-        fileList = self.interface.getCSVFileList(filterInvalid=True)
-        self.assertEqual(len(fileList) , 5)
+    def testUniqueKeys(self):
+        '''See that we can get the unique columns in SQL database'''
+        out = self.interface.pullUniqueKeys('STATION')
+        self.assertTrue(len(out) > 2)
         
-    def test_lableSetters(self):
-        '''See if we can set the x, y lables'''
-        self.assertEqual(self.interface.categoryLable, 'STATION')
-        self.assertEqual(self.interface.xLable, 'DATE')
-        self.assertEqual(self.interface.yLable, 'TAVG')
+
         
-    def test_indexFiles(self):
-        '''See if the file indexer works'''
-        for f in self.interface.csvFiles:
-            if f.validLables:
-                self.assertTrue(len(f.indexDict) > 0)
-        
-    def test_FileCounts(self):
-        '''See if the file counts work'''
-        
-        allFiles = self.interface.getFileCountAll()
-        validFiles = self.interface.getFileCountValid()
-        
-        self.assertEqual(allFiles, 6)
-        self.assertEqual(validFiles, 5)
-        
-    def test_getNameData(self):
-        '''See if we can load a data set'''
-        stations = self.interface.getCategoryList()
-        for station in stations:
-            data = self.interface.getCategoryDataset(station)
-            self.assertTrue(data.name in stations)
-            
 if __name__ == "__main__":
     suite = unittest.makeSuite(DataInterfaceTest)
     runner = unittest.TextTestRunner(verbosity=2)
