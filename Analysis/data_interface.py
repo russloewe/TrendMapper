31508dd835dc4106a113ea6cbb37bc92f4ef7803
Russell Loewe <russloewe@gmail.com>
2018-08-23 01:00:34 -0700

    added more tests and methods, changed some test data

--- Analysis/data_interface.py
+++ Analysis/data_interface.py
@@ -5,9 +5,6 @@ class DataInterface():
     
     def __init__(self):
         self.attributeNames = []
-    
-    def test(self):
-        pass
         
     def setAttributeNames(self, names):
         '''add attributes to an array that will be copied from 
@@ -19,22 +16,45 @@ class DataInterface():
     
     def initSQL(self):
         self.con = sqlite3.connect(":memory:")
-        self.con.enable_load_extension(True)
+        self.conout = sqlite3.connect(":memory:")
+        self.conmeta = sqlite3.connect(":memory:")
         curR = self.con.cursor()
+        cur2 = self.conout.cursor()
+        cur3 = self.conmeta.cursor()
         if len(self.attributeNames) < 1:
             raise AttributeError("no attributes specified, cant init SQL")
-        curR.execute("CREATE TABLE CSVdata (dummy int)")
+        curR.execute("CREATE TABLE CSVdata (ID int PRIMARY KEY)")
+        cur2.execute("CREATE TABLE resultdata (ID int)")
+        cur3.execute("CREATE TABLE metadata (filename VARCHAR(200) UNIQUE)")
         for name in self.attributeNames:
             curR.execute("ALTER TABLE CSVdata ADD {} VARCHAR(50)".format(name))
+            cur2.execute("ALTER TABLE resultdata ADD {} VARCHAR(50)".format(name))
         self.con.commit()
-
+        self.conout.commit()
+        self.conmeta.commit()
+   
+    def close(self):
+        '''close the sql connection'''
+        self.con.close()
+        self.con = None
+    
+    def connectSQL(self, path):
+        self.con = sqlite3.connect(path)
+    
     def loadCSV(self, filePath):
         '''take the path of a csv file and import the rows into a sql 
         database'''
         try:
             cur = self.con.cursor()
+            curmeta = self.conmeta.cursor()
         except AttributeError:
             raise AttributeError('SQL connection not established')
+        try:
+            #if the file has been loaded, skip
+            curmeta.execute('INSERT INTO metadata (filename) VALUES (?)', (filePath,))
+        except sqlite3.IntegrityError:
+            return False
+        
         with open(filePath, 'rb') as csvfile:
             dr = csv.DictReader(csvfile) # comma is default delimiter
             for i in dr:
@@ -50,7 +70,7 @@ class DataInterface():
         self.con.commit()
         return True
     
-    def writeDataOut(self, path):
+    def writeResultsToCSV(self, path):
          with open(path, 'w') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -78,6 +98,41 @@ class DataInterface():
             raise AttributeError('SQL connection not established')
         cur.execute('SELECT DISTINCT {} from CSVdata'.format(column))
         for row in cur:
-            keyList.append(row)
+            keyList.append(str(row[0]))
         return keyList
         
+    def pullXYData(self, keyCol, keyName, xName, yName):
+        '''Get data set for specific keyname'''
+        data = []
+        try:
+            cur = self.con.cursor()
+        except AttributeError:
+            raise AttributeError('SQL connection not established')
+        cur.execute('SELECT {}, {} FROM CSVdata WHERE {} == "{}"'.format(xName, yName, keyCol, keyName))
+        for row in cur:
+            x = str(row[0])
+            y = str(row[1])
+            data.append((x, y))
+        return data
+
+    def saveMainToDB(self, path, overwrite=False):
+        '''save the main data table 'CSVdata' to SQL db on disk, 
+        takes filename'''
+        
+        newdb = sqlite3.connect(path)
+        if overwrite:
+            newdb.execute("drop table if exists CSVdata")
+        query = "".join(line for line in self.con.iterdump())
+        newdb.executescript(query)
+        
+    def saveResultsToDB(self, path, overwrite=False):
+        '''save the main data table 'CSVdata' to SQL db on disk, 
+        takes filename'''
+        
+        newdb = sqlite3.connect(path)
+        if overwrite:
+            newdb.execute("drop table if exists resultdata")
+        query = "".join(line for line in self.conout.iterdump())
+        newdb.executescript(query)
+
+           
--- Analysis/test/test_datainterface.py
+++ Analysis/test/test_datainterface.py
@@ -12,7 +12,7 @@ __author__ = 'russloewe@gmai.com'
 __date__ = '2018-07-28'
 __copyright__ = 'Copyright 2018, Russell Loewe'
 
-import unittest, sqlite3
+import unittest, sqlite3, os
 
 from Analysis.data_interface import DataInterface
 
@@ -30,12 +30,35 @@ class DataInterfaceTest(unittest.TestCase):
 
     def tearDown(self):
         """Runs after each test."""
+        self.interface.close()
         self.interface = None
+        try:
+            os.remove('./Analysis/test/testout.csv')
+        except:
+            pass
+        try:
+            os.remove('./Analysis/test/testout.sqlite')
+        except:
+            pass
+    
+    def test_loadMultipleFiles(self):
+        '''Make sure a file cant be loaded twice'''
+        val = self.interface.loadCSV('./Analysis/test/1.csv')
+        self.assertFalse(val)
+    
+    def test_loadIncompatableFiles(self):
+        '''Make sure that we dont load a file with bad columns'''
+        tmp = DataInterface()
+        names = ['DATE', 'STATION', 'TAVG', 'NAME', 'LATITUDE', 'LONGITUDE']
+        tmp.setAttributeNames(names)
+        tmp.initSQL()
+        val = tmp.loadCSV('./Analysis/test/s2.csv')
+        self.assertFalse(val)
         
-    def testUniqueKeys(self):
+    def test_UniqueKeys(self):
         '''See that we can get the unique columns in SQL database'''
         out = self.interface.pullUniqueKeys('STATION')
-        self.assertTrue(len(out) > 20)
+        self.assertEqual(len(out) , 62)
         try:
             self.interface.pullUniqueKeys('TTT')
         except sqlite3.OperationalError:
@@ -49,6 +72,46 @@ class DataInterfaceTest(unittest.TestCase):
         for name in ['DATE', 'STATION', 'TAVG', 'NAME', 'LATITUDE', 'LONGITUDE']:
             self.interface.pullUniqueKeys(name)
 
+    def test_pullxydata(self):
+        '''test that we can pull xy data for a station'''
+        data = self.interface.pullXYData('STATION', 'USR0000WALD', 'DATE', 'TAVG')
+        expected = [('1989', '43.6'), ('1995', '45.6'), ('2000', '45.1'), 
+                    ('2001', '46.3'), ('2003', '47.3'), ('2005', '46.9'), 
+                    ('2006', '46.7'), ('2007', '46.3'), ('2008', '45.2'), 
+                    ('2009', '45.8'), ('2010', '45.7'), ('2011', '44.8'), 
+                    ('2013', '46.8'), ('2016', '47.7'), ('2017', '46.9')]
+                    
+        intersect1 = set(data).symmetric_difference(expected)
+        intersect2 = set(expected).symmetric_difference(data)
+        self.assertEqual(len(intersect1), 0)
+        self.assertEqual(len(intersect2), 0)
+        
+    def test_nosql(self):
+        '''Make sure that error is thrown if we forget to init sql or 
+        set attributes'''
+        tmp = DataInterface()
+        try:
+            tmp.loadFolder('./Analysis/test/')
+        except AttributeError:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
+        
+    def test_noattr(self):
+        '''Make sure error is raised trying to init sql before
+        attribute'''
+        tmp = DataInterface()
+        try:
+            tmp.initSQL()
+        except AttributeError:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
+        
+    def test_writecsv(self):
+        '''make sure we can write out a csv'''
+        self.interface.writeResultsToCSV('./Analysis/test/testout.csv')
+        
 
         
 if __name__ == "__main__":
