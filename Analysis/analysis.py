2df3c575e3ab8ab3b020e81ecb12847244c8f3f4
Russell Loewe <russloewe@gmail.com>
2018-08-29 18:46:08 -0700

    refactored analysis.py and updated tests

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,148 +1,60 @@
 import csv
 import numpy
 from sets import Set
-#from Analysis.data_interface import DataInterface
-from data_interface import DataInterface
-class Analysis():
-    def __init__(self):
-        '''linear outliers is a list of names of unique keys for statistical
-        outliers, defalut 2 time std dev from mean. Init as None so we
-        don't mistakenly think there are no outliers when we just haven't ran 
-        the computation yet'''
-        self.show_warnings = True
-        self.skipped_datapoints = 0
-        self.row_count = 0
-    
-    def calculateLinearRegression(self, dataSet):
-        '''take an array of tuples, [(x1,y1), (x2,y2) ..., (xn,yn)]
-           reorganize as two arrays [x1, x2 ..., xn] and [y1, y2, ..., yn]
-           and use numpy least squares to find linear best fit coeeficients and
-           return the slope and intercept for the best fit line and rank'''
-        x = [] #init independent var array
-        y = [] #init dependent var array
-        for data_point in dataSet:
-            x_val,y_val = data_point  #unpack tuple
-            x.append(x_val)
-            y.append(y_val)
-        if len(x) != len(y):
-            print "something went wrong, x y dim missmath"
-            return None
-        A = numpy.vstack([x, numpy.ones(len(x))]).T
-        linearFitResult = numpy.linalg.lstsq(A,y)
-        slope, intercept = linearFitResult[0]
-        rank = linearFitResult[2]
-        results = {'slope' : slope, 'intercept': intercept, 'rank' : rank, 'setsize' : len(dataSet)}
-        return(results)
-           
-    def linearFitDataSeries(self, dataSet):
-        '''take the dataSeries object and run it through the regression
-        function and put the result into the dataSeries attributes'''
-        results = self.calculateLinearRegression(dataSet.dataPoints)
-        for key in results:
-            dataSet.addStat(key, results[key])
-        return(dataSet)
 
-            
-    def calculatePopulationStats(self, dataArray, key):
-        '''take the results from the linear calc, rearrange them to list
-        of names and slopes. then find entries outside of threshold * standard
-        deviation'''
-        popSample = []
-        for data in dataArray:
-            popSample.append(data.dataStats[key])
-        popMean = numpy.mean(popSample)
-        popStd = numpy.std(popSample)
-        
-        for data in dataArray:
-            keyVal = data.dataStats[key]
-            
-            zScore = (popMean - keyVal) / popStd
-            sqrtN = numpy.sqrt(data.dataStats['setsize'])
-            tScore = (popMean - keyVal) / (popStd / sqrtN)
-            data.addStat('{}_zscore'.format(key) , zScore)
-            data.addStat('{}_tscore'.format(key) , tScore)
-            data.addStat('{}_mean'.format(key) , popMean)
-            data.addStat('{}_std'.format(key) , popStd)
+def calculateLinearRegression(dataSet, includeStats=False):
+    '''take an array of tuples, [(x1,y1), (x2,y2) ..., (xn,yn)]
+       reorganize as two arrays [x1, x2 ..., xn] and [y1, y2, ..., yn]
+       and use numpy least squares to find linear best fit coeeficients and
+       return the slope and intercept for the best fit line and rank'''
+    x = [] #init independent var array
+    y = [] #init dependent var array
+    if len(dataSet) < 1:
+        raise ValueError('No data points in dataset') 
+    for data_point in dataSet:
+        x_val,y_val = data_point  #unpack tuple
+        x.append(x_val)
+        y.append(y_val)
+    if len(x) != len(y):
+        raise ValueError('Dimension mismatch on x,y series')
+    A = numpy.vstack([x, numpy.ones(len(x))]).T
+    linearFitResult = numpy.linalg.lstsq(A,y)
+    slope, intercept = linearFitResult[0]
+    if includeStats:
+        rank = linearFitResult[2]
+        risidualSum = calculateRisidualSum(dataSet,slope , intercept)
+        results = {'slope' : slope, 'intercept': intercept, 'rank' : rank, 'risidualSum': risidualSum, 'setsize' : len(dataSet)}
+    else:
+         results = {'slope' : slope, 'intercept': intercept}
+    return(results)
+
+def mean(data):
+    x = [] #init independent var array
+    y = [] #init dependent var array
+    if len(data) < 1:
+        raise ValueError('No data points in dataset') 
+    for data_point in data:
+        x_val,y_val = data_point  #unpack tuple
+        x.append(x_val)
+        y.append(y_val)
+    if len(x) != len(y):
+        print "something went wrong, x y dim missmath"
+        return None
+    a = numpy.mean(y)
+    results = {'mean' : a}
+    return(results)
     
-    def calculateRisiduals(self, dataSeries):
-        '''Take a dataseries and calculate the difference in the real value
-        for each year in the set with the value we get by using our linear model'''
-        risidual = 0
-        slope = dataSeries.dataStats['slope']
-        intercept = dataSeries.dataStats['intercept']
-        for dataPoint in dataSeries.dataPoints: 
-            x, y = dataPoint
-            risd = (x * slope + intercept) - y
-            risidual += risd
-        dataSeries.addStat('risidual', risidual)
-        return(dataSeries)
-            
-def runner():
-    interface = DataInterface()
-    interface.loadFolder('./test/')
-    interface.setCategoryLable('STATION')
-    interface.setXLable('DATE')
-    interface.setYLable('TAVG')
-    interface.addCopyAttributeLable('LATITUDE')
-    interface.addCopyAttributeLable('LONGITUDE')
-    interface.indexCSVFiles()
-    interface.indexCategories()
-    testData = []
-    stations = interface.getCategoryList()
-    for station in stations:
-        data = interface.getCategoryDataset(station)
-        testData.append(data)
     
-    analysis = Analysis()
-    result1 = analysis.linearFitDataSeries(testData[0])
-    result1 = analysis.calculateRisiduals(testData[0])
-    print result1.dataStats['slope']
-    print result1.dataStats['risidual']
+def calculateRisidualSum(dataSet, slope, intercept):
+    '''Take a dataseries and calculate the difference in the real value
+    for each year in the set with the value we get by using our linear model'''
+    risidual = 0
+    for dataPoint in dataSet: 
+        x, y = dataPoint
+        risd = (x * slope + intercept) - y
+        risidual += risd
+    return(risidual)
 
     
 if __name__ == "__main__":
-    import os
-    import cProfile
-    cProfile.run('runner()')
-    
-   # d = DataInterface()
-   # d.addCSVFile('1.csv')
-   # d.addCSVFile('2.csv')
-   # d.addCSVFile('3.csv')
-    #d.addCSVFile('4.csv')
-
-   # d.addCSVFile('s1.csv')
-    #d.addCSVFile('s2.csv')
-   # d.addCopyAttributeLable('LATITUDE')
-   # d.name = 'STATION'
-    #d.xLable = 'DATE'
-   # d.yLable = 'TAVG'
-    #d.indexCSVFiles()
-    #for u in d.CSV:
-      #  print u.name, u.indexDict
-    
-    #stations = d.getCSVNameList()
-   # print stations
-    #data = d.getCSVNameData(stations[0])
-    #print data.name, data.dataPoints, data.copyAtttributes
-    #print d.getCSVCount()
-  
-    if 1 == 2:
-        analysis = Analysis( )  #TAVG_mtrix.csv
-        analysis.set_unique_vale('STATION')
-        analysis.set_independent_var('DATE')
-        analysis.set_dependent_var('DX90') 
-        analysis.show_warnings = False
-        analysis.import_CSV('all.csv')
-        analysis.calculate_linear_fit(discard_badfit = True)
-        #analysis.print_slopes()
-        analysis.find_outliers()
-        analysis.computRegressionStats()
-        analysis.write_linear_fit_CSV("all_TAVG.csv", export_risduals = True, write_CSVT = True, filter_outliers = True)
-        analysis.computRegressionStats()
- 
-
-
-
-
-
+    pass
--- Analysis/test/test_analysis.py
+++ Analysis/test/test_analysis.py
@@ -15,7 +15,7 @@ __copyright__ = 'Copyright 2018, Russell Loewe'
 import unittest
 
 from Analysis.data_interface import DataInterface
-from Analysis.analysis import Analysis
+from Analysis.analysis import *
 
 
 class AnalysisTest(unittest.TestCase):
@@ -23,98 +23,81 @@ class AnalysisTest(unittest.TestCase):
 
     def setUp(self):
         """Runs before each test."""
+        self.testData = [[(x, x) for x in range(10)],
+                         [(x, x*2) for x in range(-10,10)],
+                         [(x, x*3+4) for x in range(10)],
+                         [(1,1)]
+                         ]
         
-        #init the data interface to generate test data for the
-        #analysis module
-        self.interface = DataInterface()
-        self.interface.loadFolder('./Analysis/test/')
-        self.interface.setCategoryLable('STATION')
-        self.interface.setXLable('DATE')
-        self.interface.setYLable('TAVG')
-        self.interface.addCopyAttributeLable('LATITUDE')
-        self.interface.addCopyAttributeLable('LONGITUDE')
-        self.interface.indexCSVFiles()
-        self.interface.indexCategories()
-        self.testData = []
-        stations = self.interface.getCategoryList()
-        for station in stations:
-            data = self.interface.getCategoryDataset(station)
-            self.testData.append(data)
-        
-        self.analysis = Analysis()
     
     def test_linearRegression(self):
         '''Test that linear regression passes some test calculation'''
-        testData1 = [(x, x) for x in range(10)]
-        testData2 = [(x, x*2) for x in range(10)]
-        testData3 = [(x, x*3+4) for x in range(10)]
-        testData4 = [(x, x) for x in range(1)]
-        result1 = self.analysis.calculateLinearRegression(testData1)
-        result2 = self.analysis.calculateLinearRegression(testData2)
-        result3 = self.analysis.calculateLinearRegression(testData3)
-        result4 = self.analysis.calculateLinearRegression(testData4)
-        self.assertTrue(abs(result1['slope'] - 1.0) < .0000001)
-        self.assertTrue(abs(result1['intercept'] -0) < .0000001)
+        testData0 = self.testData[0]
+        testData1 = self.testData[1]
+        testData2 = self.testData[2]
+        testData3 = self.testData[3]
+        result0 = calculateLinearRegression(testData0, includeStats = True)
+        result1 = calculateLinearRegression(testData1, includeStats = True)
+        result2 = calculateLinearRegression(testData2, includeStats = True)
+        result3 = calculateLinearRegression(testData3, includeStats = True)
+        
+        self.assertTrue(abs(result0['slope'] - 1.0) < .0000001)
+        self.assertTrue(abs(result0['intercept'] -0) < .0000001)
+        self.assertTrue(abs(result0['risidualSum'] -0) < .0000001)
+        self.assertEqual(result0['rank'], 2)
+        
+        self.assertTrue(abs(result1['slope'] -2) < .0001)
+        self.assertTrue(abs(result1['intercept'] -0) < .0001)
+        self.assertTrue(abs(result1['risidualSum'] -0) < .0000001)
         self.assertEqual(result1['rank'], 2)
         
-        self.assertTrue(abs(result2['slope'] -2) < .0001)
-        self.assertTrue(abs(result2['intercept'] -0) < .0001)
+        self.assertTrue(abs(result2['slope'] -3) < .0001)
+        self.assertTrue(abs(result2['intercept'] -4) < .0001)
+        self.assertTrue(abs(result2['risidualSum'] -0) < .0000001)
         self.assertEqual(result2['rank'], 2)
+    
+    def test_rank(self):
+        '''Make sure that right rank is returned'''
+        result = calculateLinearRegression(self.testData[3], includeStats = True)
+        self.assertEqual(result['rank'], 1)
         
-        self.assertTrue(abs(result3['slope'] -3) < .0001)
-        self.assertTrue(abs(result3['intercept'] -4) < .0001)
-        self.assertEqual(result3['rank'], 2)
+    def test_linStats(self):
+        '''make sure that stats are not returned for default params'''
         
-        self.assertEqual(result4['rank'], 1)
-        #test on real data
-        data = self.testData[0]
-        data = self.analysis.linearFitDataSeries(data)
-        data = self.analysis.calculateRisiduals(data)
-        self.assertTrue(abs(data.dataStats['slope'] - 0.041034923339) < 0.00000001)
+        result0 = calculateLinearRegression(self.testData[3])
+        self.assertTrue('rank' not in result0)
         
-    def test_dataLinReg(self):
-        '''Test that analysis can process a dataSeries'''
-        data = self.testData[0]
-        data.dataPoints = [(x, x) for x in range(10)]
-        data1 = self.analysis.linearFitDataSeries(data)
+    def test_linBadArray(self):
+        '''make sure right exception is raised for bad data arrays'''
         
-        self.assertTrue(abs(data1.dataStats['slope'] - 1.0) < .0000001)
-        self.assertTrue(abs(data1.dataStats['intercept'] -0) < .0000001)
-        self.assertEqual(data1.dataStats['rank'], 2)
-        
+        data1 = [(1,1), (2,) , (3,9)]
+        data2 = []
+        try:
+            calculateLinearRegression(data1)
+            calculateLinearRegression(data2)
+        except ValueError:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
+    
     def test_calcRisiduals(self):
         '''Make sure we can find the risidual'''
         
-        data1 = self.testData[0]
-        data2 = self.testData[1]
-        data2.dataPoints = [(x, x) for x in range(10)]
-        data1 = self.analysis.linearFitDataSeries(data1)
-        data1 = self.analysis.calculateRisiduals(data1)
-        data2 = self.analysis.linearFitDataSeries(data2)
-        data2 = self.analysis.calculateRisiduals(data2)
-        
-        self.assertTrue(abs(data1.dataStats['risidual'] - 0) < 0.00000001)
-        
-        self.assertTrue(abs(data2.dataStats['risidual'] - 0) < 0.00000001)
-        
-        
-    def test_popStats(self):
-        '''Test that pop stats are correct'''
-        for dataset in self.testData:
-            self.analysis.linearFitDataSeries(dataset)
-            
-        self.analysis.calculatePopulationStats(self.testData, 'slope')
-        
-        self.assertTrue('slope_mean' in self.testData[0].dataStats)
+        data0 = self.testData[0]
+        data1 = self.testData[1]
+        data2 = self.testData[2]
+        data0Ris = calculateRisidualSum(data0, 1, 0)
+        data1Ris = calculateRisidualSum(data1, 2, 0)
+        data2Ris = calculateRisidualSum(data2, 3, 4)
+        self.assertTrue(abs(data0Ris - 0) < 0.00000001)
+        self.assertTrue(abs(data1Ris - 0) < 0.00000001)
+        self.assertTrue(abs(data2Ris - 0) < 0.00000001)
 
         
     def tearDown(self):
         """Runs after each test."""
         self.interface = None
         
-    def test_loadFolder(self):
-        '''Test load folder...'''
-        pass
 
         
 
