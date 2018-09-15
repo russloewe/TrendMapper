ea404eaa93994a7d905a7fa41044986e60370f9c
Russell Loewe <russloewe@gmail.com>
2018-09-14 17:41:30 -0700

    changed param of calculateLinearRegression to take two lists insted of a list of points

--- analysis.py
+++ analysis.py
@@ -2,22 +2,11 @@ import csv
 import numpy
 from sets import Set
 
-def calculateLinearRegression(dataSet, includeStats=False):
+def calculateLinearRegression(x, y, includeStats=False):
     '''take an array of tuples, [(x1,y1), (x2,y2) ..., (xn,yn)]
        reorganize as two arrays [x1, x2 ..., xn] and [y1, y2, ..., yn]
        and use numpy least squares to find linear best fit coeeficients and
        return the slope and intercept for the best fit line and rank'''
-    x = [] #init independent var array
-    y = [] #init dependent var array
-    if len(dataSet) < 1:
-        raise ValueError('No data points in dataset') 
-    for data_point in dataSet:
-        x_val,y_val = data_point  #unpack tuple
-        if (x_val == '') or (y_val == ''):
-            pass
-        else:
-            x.append(x_val)
-            y.append(y_val)
     A = numpy.vstack([x, numpy.ones(len(x))]).T
     linearFitResult = numpy.linalg.lstsq(A,y)
     slope, intercept = linearFitResult[0]
