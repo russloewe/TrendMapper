6356a7752657bc1f80ebdaa3103aaf7582315862
Russell Loewe <russloewe@gmail.com>
2018-09-19 20:36:31 -0700

    moved class TrendMapperProcess to own file

--- trend_mapper.py
+++ trend_mapper.py
@@ -31,7 +31,7 @@ from analysis import calculateLinearRegression
 from trend_mapper_tools import *
 # Import the code for the dialog
 from trend_mapper_dialog import TrendMapperDialog
-from trend_mapper_status import TrendMapperStatus
+from trend_mapper_process import TrendMapperProcess
 import os.path
 #import the custom logger
 from time import sleep
@@ -269,85 +269,3 @@ class TrendMapper:
         self.dlg.abortButton.clicked.connect(tmprocess.abort)
         tmprocess.start()   
         
-class TrendMapperProcess(QThread):
-    def __init__(self, newLayer, stations, xField, yField, copyAttr):
-        super(TrendMapperProcess, self).__init__()
-        self.counter = 0
-        self.totalCounter = len(stations)
-        self.stations = stations
-        self.copyAttr = copyAttr
-        self.dataAttr = [xField, yField]
-        self.xField = xField
-        self.yField = yField
-        self.newLayer = newLayer
-        self.running = True
-        self.stopSig = QtCore.SIGNAL("stopSignal")
-        self.progSig = QtCore.SIGNAL("progress")
-        self.msgSig = QtCore.SIGNAL ("msgSignal")
-        self.abortSig = QtCore.SIGNAL('abortSignal')
-
-            
-    def abort(self):
-        self.running = False
-        self.emit(self.abortSig)
-        self.emit(self.msgSig, 'Abort Called')
-        
-            
-    def createConvFunction(self, formatDateCol, dateCol, dateForm):
-        if formatDateCol:
-            convFunction = convFunNum(self.dataAttr, dateColumn = dateCol,
-                                            dateFormat = dateForm)
-        else:
-            convFunction = convFunNum(self.dataAttr)
-        self.convFunction = convFunction
-        
-    def createDataFunction(self, layer, keyCol, statsCheck):
-        def getdata(station):
-            featureItr = featureGenerator(layer, station, keyCol)
-            pointItr = datapointGenerator(featureItr, self.copyAttr + self.dataAttr)
-            filterItr = filterDatapointGenerator(pointItr, filterFun)
-            convItr = convertedDatapointGenerator(filterItr, 
-                                       self.convFunction,
-                                       skipOnErr = True)
-            data = organizeData(convItr, self.dataAttr)
-            for key in self.dataAttr:
-                if (len(data[self.xField]) < 1) or (len(data[self.yField]) < 1):
-                    return data, None
-            result = calculateLinearRegression(data[self.xField], 
-                                        data[self.yField],
-                                    includeStats = statsCheck)
-            return data, result
-        self.getData = getdata
-        
-    def run(self):
-        newFeatures = []
-        self.firstRun = True
-        for station in self.stations:
-            if self.running == False:
-                break
-            else:
-                self.process(station)
-        self.emit(self.stopSig)
-        
-    def process(self, station):
-        self.counter += 1
-        self.updateProgress('Processing Datasets: ({})'.format(self.counter))
-        #get result for one station
-        data, result = self.getData(station)
-        if result == None:
-            return
-        if self.firstRun:
-            #add the add the result attributes as fields in new layer
-            addResultFields(self.newLayer, result)
-            firstRun = False
-        mergeResult = mergeDicts(data, result, excluded = self.dataAttr)
-        newFeature = makeFeature(self.newLayer, mergeResult)
-        checkTrue(self.newLayer.startEditing())
-        self.newLayer.addFeatures([newFeature], makeSelected = False)
-        checkTrue(self.newLayer.commitChanges())
-
-    def updateProgress(self, msg):
-        progress = int(self.counter / float(self.totalCounter) * 100)
-        self.emit(self.progSig, progress, msg)
-    
-
