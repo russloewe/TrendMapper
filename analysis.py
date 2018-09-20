5dfda650026cb7c6ef598eedd44d3bd7b04a2d0f
Russell Loewe <russloewe@gmail.com>
2018-09-20 16:23:47 -0700

    changed the comment style in trend_mapper_tools and analysis so that the header is not included in the docs

--- analysis.py
+++ analysis.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- TrendMapperDialog
-                                 A QGIS plugin
- calculate trendlines along catagories
-                             -------------------
-        begin                : 2018-07-28
-        git sha              : $Format:%H$
-        copyright            : (C) 2018 by Russell Loewe
-        email                : russloewe@gmai.com
- ***************************************************************************/
+# /***************************************************************************
+ # TrendMapperDialog
+                                 # A QGIS plugin
+ # calculate trendlines along catagories
+                             # -------------------
+        # begin                : 2018-07-28
+        # git sha              : $Format:%H$
+        # copyright            : (C) 2018 by Russell Loewe
+        # email                : russloewe@gmai.com
+ # ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
+# /***************************************************************************
+ # *                                                                         *
+ # *   This program is free software; you can redistribute it and/or modify  *
+ # *   it under the terms of the GNU General Public License as published by  *
+ # *   the Free Software Foundation; either version 2 of the License, or     *
+ # *   (at your option) any later version.                                   *
+ # *                                                                         *
+ # ***************************************************************************/
+
 import csv
 import numpy
 from sets import Set
